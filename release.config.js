f9bd5b242cc61c6f51ff8c07ab3e13ccd4250cd0
Severin Ibarluzea <seveibar@gmail.com>
2023-07-10 16:20:18 -0700

    fix: add release config

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/npm", { npmPublish: true }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+}
