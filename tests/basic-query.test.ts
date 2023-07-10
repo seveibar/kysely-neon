261292afb90ba67e1e0b6d58a53f0d949542d319
igalklebanov <igalklebanov@gmail.com>
2023-07-10 23:40:38 +0300

    test both dialects.

--- tests/basic-query.test.ts
+++ tests/basic-query.test.ts
@@ -1,45 +1,62 @@
 import test from "ava"
-import { Kysely } from "kysely"
+import { Generated, Kysely, sql } from "kysely"
 import ws from "ws"
-import { NeonDialect } from "../src"
+import { NeonDialect, NeonHTTPDialect } from "../src"
 
-test("test query with neon", async (t) => {
-  const db = new Kysely<{ kysely_neon_test: { id: string; email: string } }>({
-    dialect: new NeonDialect({
-      connectionString: process.env.NEON_DATABASE_URL,
-      webSocketConstructor: ws,
-    }),
-  })
+const connectionString = process.env.NEON_DATABASE_URL!
 
-  await db.schema.dropTable("kysely_neon_test").ifExists().execute()
+const contexts = [
+  {
+    dialect: new NeonDialect({ connectionString, webSocketConstructor: ws }),
+    name: NeonDialect.name,
+    supportsTransactions: true,
+  },
+  {
+    dialect: new NeonHTTPDialect({ connectionString }),
+    name: NeonHTTPDialect.name,
+    supportsTransactions: false,
+  },
+] as const
 
-  await db.schema
-    .createTable("kysely_neon_test")
-    .ifNotExists()
-    .addColumn("id", "varchar", (col) => col.primaryKey())
-    .addColumn("email", "varchar")
-    .execute()
+for (const ctx of contexts) {
+  test(ctx.name, async (t) => {
+    const db = new Kysely<{
+      kysely_neon_test: { id: Generated<number>; email: string }
+    }>({
+      dialect: ctx.dialect,
+    })
 
-  await db
-    .insertInto("kysely_neon_test")
-    .values({ id: "123", email: "info@example.com" })
-    .execute()
+    await db.schema.dropTable("kysely_neon_test").ifExists().execute()
 
-  await db.transaction().execute(async (trx) => {
-    await trx
-      .insertInto("kysely_neon_test")
-      .values({ id: "456", email: "info@example.com" })
+    await db.schema
+      .createTable("kysely_neon_test")
+      .ifNotExists()
+      .addColumn("id", "serial", (col) => col.primaryKey())
+      .addColumn("email", "varchar")
       .execute()
 
-    await trx
+    await db
       .insertInto("kysely_neon_test")
-      .values({ id: "789", email: "info@example.com" })
+      .values({ email: "info@example.com" })
       .execute()
+
+    if (ctx.supportsTransactions) {
+      await db.transaction().execute(async (trx) => {
+        await trx
+          .insertInto("kysely_neon_test")
+          .values({ email: "info@example.com" })
+          .execute()
+
+        await trx
+          .insertInto("kysely_neon_test")
+          .values({ email: "info@example.com" })
+          .execute()
+      })
+    }
+
+    const result = await db.selectFrom("kysely_neon_test").selectAll().execute()
+
+    t.truthy(result)
+    t.truthy(result[0].email)
   })
-
-  const result = await db.selectFrom("kysely_neon_test").selectAll().execute()
-
-  t.truthy(result)
-  t.is(result.length, 3)
-  t.truthy(result[0].email)
-})
+}
